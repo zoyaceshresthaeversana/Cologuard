a978861f1f8f86689a82a0dbfef9afc9a21eb020
Zoyace Shrestha <Zoyace.Shrestha@macbookpro-c02fn2y3md6t.lan>
2023-07-20 16:54:21 -0400

    Hidden section completed

--- JS/style.js
+++ JS/style.js
@@ -1,9 +1,19 @@
 const commercialBtn = document.querySelector("#Commercial");
 const hiddenBlock = document.querySelector(".hidden-chart-container ");
+const navBar = document.querySelector(".navbar-container-mobile-view");
+const navBarIcon = document.querySelector(".navbar-hamburger-icon");
 
 function toggleMobileMenu(menu) {
   menu.classList.toggle("open");
 }
+
+// navBarIcon.addEventListener("click", () => {
+//   if (navBar.style.display === "none") {
+//     navBar.style.display = "flex";
+//   } else {
+//     navBar.style.display = "none";
+//   }
+// });
 
 commercialBtn.addEventListener("click", () => {
   if (hiddenBlock.style.display === "none") {
