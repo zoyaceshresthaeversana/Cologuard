fd6dd0e21fdf2c8c14ecc8cdc1910ecbe46b390c
Zoyace Shrestha <Zoyace.Shrestha@macbookpro-c02fn2y3md6t.lan>
2023-07-20 18:15:31 -0400

    funcatonality completed

--- JS/style.js
+++ JS/style.js
@@ -2,6 +2,9 @@ const commercialBtn = document.querySelector("#Commercial");
 const hiddenBlock = document.querySelector(".hidden-chart-container ");
 const navBar = document.querySelector(".navbar-container-mobile-view");
 const navBarIcon = document.querySelector(".navbar-hamburger-icon");
+const searchBtn = document.querySelector(".CommercialSearch");
+const coverContainer = document.querySelector(".hidden-cover-container");
+const nonCoverContainer = document.querySelector(".hidden-non-cover-container");
 
 function toggleMobileMenu(menu) {
   menu.classList.toggle("open");
@@ -20,5 +23,20 @@ commercialBtn.addEventListener("click", () => {
     hiddenBlock.style.display = "grid";
   } else {
     hiddenBlock.style.display = "none";
+  }
+});
+
+searchBtn.addEventListener("click", () => {
+  var selectedCounty = document.getElementById("County").value;
+  var selectedProvider = document.getElementById("Provider").value;
+  console.log(selectedCounty);
+  console.log(selectedProvider);
+  if (selectedCounty == "QA" && selectedProvider == "EBB") {
+    nonCoverContainer.style.display = "none";
+    coverContainer.style.display = "block";
+  } else if (selectedCounty == "QA" && selectedProvider == "MP") {
+    nonCoverContainer.style.display = "block";
+    coverContainer.style.display = "none";
+  } else {
   }
 });
