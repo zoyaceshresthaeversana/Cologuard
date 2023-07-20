c844a5e09b5ca3d9084a1f686e9897b8ea066d7c
Zoyace Shrestha <Zoyace.Shrestha@macbookpro-c02fn2y3md6t.lan>
2023-07-20 16:10:49 -0400

    Hidden section completed

--- JS/style.js
+++ JS/style.js
@@ -1,3 +1,14 @@
+const commercialBtn = document.querySelector("#Commercial");
+const hiddenBlock = document.querySelector(".hidden-chart-container ");
+
 function toggleMobileMenu(menu) {
   menu.classList.toggle("open");
 }
+
+commercialBtn.addEventListener("click", () => {
+  if (hiddenBlock.style.display === "none") {
+    hiddenBlock.style.display = "grid";
+  } else {
+    hiddenBlock.style.display = "none";
+  }
+});
